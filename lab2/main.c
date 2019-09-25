60599f23cf960cfd82724fbf2556ab3f411197b3
Demidov Danil <fl41demidov@outlook.com>
2019-09-25 21:26:13 +0700

    add in/out array function

--- lab2/main.c
+++ lab2/main.c
@@ -1,6 +1,53 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include <mcheck.h>
+#define max_len 1024
+
+char** inp_arr(int count);
+void out_arr(char **arr, int count);
+void free_arr(char **arr, int count);
 
 int main()
-{
+{	
+	char **mas = NULL; 
+	int count = 3;
+	mtrace();
+	mas = inp_arr(count);
+	out_arr(mas, count);
+	free_arr(mas, count);
+	printf("hello\n");
 	return 0;
+}
+
+char** inp_arr(int count)
+{
+	char buf[max_len];
+	char **arr;  
+	arr = (char **)malloc(sizeof(char *)*count);
+    for (int i = 0; i < count ; i++)
+    {
+        scanf("%s", buf); 
+        arr[i] = (char *)malloc(sizeof(char)*strlen(buf)); 
+        strcpy(arr[i], buf); 
+    }
+    return arr; 
+}
+
+void out_arr(char **arr, int count)
+{
+    for (int i = 0; i < count ; i++)
+    {
+        printf("%s\n", arr[i]);
+    }
+}
+
+void free_arr(char **arr, int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+        free(arr[i]); 
+    }
+    free(arr); 
 }
